a01787922424a7c2a168531775a0a42b51eec5d3
SeongHyokyung <rosa0000918@gmail.com>
2024-03-28 15:26:45 +0900

    edit view

--- app/src/main/java/com/example/hellotoastapplication/MainActivity.java
+++ app/src/main/java/com/example/hellotoastapplication/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.hellotoastapplication;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
